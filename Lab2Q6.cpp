a54937b3d378649a7e443bdaa5988cdb92a2852f
Musawer Gul dahri <151806241+MusawerGul@users.noreply.github.com>
2024-10-31 20:19:21 +0500

    Add files via upload

---
+++ Lab2Q6.cpp
@@ -0,0 +1,13 @@
+//Q6:- Write a C++ program to deletes Item from bottom of the array.Print all elements of the array.
+#include <iostream>
+using namespace std;
+
+void del (int nums [] , int &size) {
+    size--;
+}
+int main(){
+    int nums [] = {1,2,3,4,4};
+    int len = sizeof(nums) / sizeof(nums[1]);
+    del (nums , len);
+    for (int i = 0; i < len; i++) cout << nums[i] << " ";
+}
